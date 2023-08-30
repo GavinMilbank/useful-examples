3904e6c4764cddb4a359e41c64586530f0d53739
Juanruz <101671986+Juanruz@users.noreply.github.com>
2023-08-29 22:15:47 -0300

    Add files via upload

---
+++ Automovel.java
@@ -0,0 +1,35 @@
+
+public class Automovel
+{
+    private String combustivel;
+    private String fabricante;
+    private int ano;
+    private String modelo;
+
+    public Automovel(String combustivel, String fabricante, int ano, String modelo)
+    {
+        this.combustivel = combustivel;
+        this.fabricante = fabricante;
+        this.ano = ano;
+        this.modelo = modelo;
+    }
+
+    public float combNecessario(float km)
+    {
+        // escreva seu código aqui
+        if (combustivel.equals("gasolina"))
+        {
+            return km / 12;
+        }
+        else if (combustivel.equals("alcool"))
+        {
+            return km / 15;
+        }
+        else if (combustivel.equals("flex"))
+        {
+             return km/30 + km/24;
+        }  
+        else 
+            return 0; // I don't know what should I return in this case
+    }
+}
